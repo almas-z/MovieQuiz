1f61a3e1e9630e17a1385b62350deb1f64e1f139
Almas <almas.zhartybayev@gmail.com>
2024-11-30 16:22:25 +0500

    Реализована верстка по макету и логика

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-extension UIColor { }
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray}
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green}
+}
